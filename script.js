58cb62915ba8c044ffb7fd980d5ab0012152db62
mdjganados <mdjganados@tip.edu.ph>
2026-01-15 18:05:47 +0800

    Add script.js and Tekken image

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('btn').addEventListener('click', function () {
+  alert('Button clicked yey! u win nothing :(');
+});
+
+document.getElementById('whoBtn').addEventListener('click', function () {
+  alert('Made by Dwyane Jorge Ganados');
+});
